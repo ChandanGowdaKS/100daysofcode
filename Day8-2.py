ab5e39ac3d72e9c05a3051c349661acae0769d36
Chandan Gowda K S <chanduchess122@gmail.com>
2024-10-02 17:20:16 +0530

    Update Day8-2.py

--- Day8-2.py
+++ Day8-2.py
@@ -23,3 +23,22 @@ def decrypt(original_text,shift_amount):
     print(decode)
 decrypt(text,shift)
 
+# Final Caesar encrypt and decrpyt
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+encode_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def caesar(original_text,shift_amount,selection):
+    if encode_decode == "decode":
+        shift_amount *= -1
+    word = ""
+    for alpha in original_text:
+        shifted_letter = alphabet.index(alpha) + shift_amount
+        shifted_letter %= len(alphabet)
+        word += alphabet[shifted_letter]
+    print(word)
+caesar(text,shift,encode_decode)
+
