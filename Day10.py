43abf788a060f0f830e156152caec0e828f5ea25
Chandan Gowda K S <chanduchess122@gmail.com>
2024-10-06 16:40:55 +0530

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -1,37 +1,8 @@
-# def name(f_name,l_name):
-#     fname = f_name.title()
-#     lname = l_name.title()
-#     print(fname,lname)
-#     return fname,lname
-#
-# name("chandu","gowda")
-'''
-def is_leap_year(year):
-    operation = []
-    operation.append(year / 4)
-    operation.append(year / 100)
-    operation.append(year / 400)
-    for op in operation:
-        if op == float(op):
-            print("Its not a Leap year")
-            # return True
-    print("Its a Leap year")
-    # return False
-
-is_leap_year(2024)'''
-from email.contentmanager import set_text_content
-
-# def my_function(a):
-#     if a > 40:
-#         return
-#         print("Terrible")
-#     if a > 80:
-#         return "Pass"
-#     else:
-#         return "Great"
-# print(my_function(25))
 
 ''' Caluculator Using Function'''
+'''My Own Code Using Only Question'''
+
+
 
 def add(n1,n2):
     return n1+n2
