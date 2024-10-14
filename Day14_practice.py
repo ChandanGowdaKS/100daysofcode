1b018a4dc2568de6480cab5459108967d224f98b
Chandan Gowda K S <chanduchess122@gmail.com>
2024-10-14 20:01:45 +0530

    Add files via upload

---
+++ Day14_practice.py
@@ -0,0 +1,83 @@
+'''Write a Python code to display numbers from a list divisible by 5 using Function'''
+
+# full_list =[]
+#
+# def num_list(num):
+#     # num = int(input("Enter The number of Numbers You want too add in list:\n"))
+#     for i in range(num):
+#         list_input = int(input("Enter The number you Want To Put in list:\n"))
+#         full_list.append(list_input)
+#     print(full_list)
+#     print("Divisible by 5 numbers are:")
+#     for full in full_list:
+#         if full % 5 == 0:
+#
+#             print(full)
+#
+# num_list(5)
+
+
+'''Exercise 7: Find the number of occurrences of a substring in a string
+Write a Python code to find how often the substring “Emma” appears in the given string.'''
+
+# srtng = "Chandan is a Software developer, Chandan Loves to build New Things, Chandan Always Try to solve Problems :"
+#
+# print(srtng.count("Chandan"))
+
+'''Print the following pattern
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+'''
+
+# for i in range(1,10):
+#     print(str(i)*i)
+
+
+'''Write a Python code to check if the given number is palindrome.
+ A palindrome number is a number that is the same after reverse. For example, 545 is the palindrome number.'''
+# number=[]
+# def palindrome(num):
+#     number.append(num)
+#     if number.reverse == num:                     NOT WORKING
+#         print("Its palindrome")
+#     else:
+#         print("Its Not a Palindrome")
+#
+# palindrome(858)
+
+# def palindrome(num):
+#     number = ""
+#     str(num)
+#     print(f"Original Number is: {num}")
+#     for i in num:
+#         number += i
+#     if num == number:
+#         print("Its palindrome")
+#     else:
+#         print("Its Not a Palindrome")
+#
+# palindrome(858)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
